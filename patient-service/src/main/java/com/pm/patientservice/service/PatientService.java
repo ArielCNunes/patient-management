c03c30312b8bff3108e918206c1e00e977e0d37d
Ariel <arielc.nunes@outlook.com>
2025-09-25 19:18:22 +0100

    Added createPatient method to service

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -1,5 +1,6 @@
 package com.pm.patientservice.service;
 
+import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
@@ -21,5 +22,17 @@ public class PatientService {
 
         return patients.stream()
                 .map(PatientMapper::toDTO).toList();
+    }
+
+    public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO) {
+        /*
+         * PatientMapper.toModel returns a Patient object.
+         * This objects gets persisted to the database through .save()
+         */
+        Patient newPatient = patientRepository.save(
+                PatientMapper.toModel(patientRequestDTO));
+
+        // Convert back to DTO and send back to controller
+        return PatientMapper.toDTO(newPatient);
     }
 }
