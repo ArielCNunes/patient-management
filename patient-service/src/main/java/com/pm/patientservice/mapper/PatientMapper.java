d0494430b6a96b954e9859b3ac3f7c51b2d793f3
Ariel <arielc.nunes@outlook.com>
2025-09-25 19:17:41 +0100

    Mapper can convert from DTO to patient object

--- patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
+++ patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
@@ -1,9 +1,16 @@
 package com.pm.patientservice.mapper;
 
+import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.model.Patient;
 
+import java.time.LocalDate;
+
 public class PatientMapper {
+    /*
+    * This method converts a patient object to a patient response DTO
+    *
+    * */
     public static PatientResponseDTO toDTO(Patient patient) {
         PatientResponseDTO patientDTO = new PatientResponseDTO();
         patientDTO.setId(patient.getId().toString());
@@ -13,5 +20,19 @@ public class PatientMapper {
         patientDTO.setDateOfBirth(patient.getDateOfBirth().toString());
 
         return patientDTO;
+    }
+
+    /*
+     * This method converts a patient response DTO to patient object
+     * */
+    public static Patient toModel(PatientRequestDTO patientRequestDTO) {
+        Patient patient = new Patient();
+        patient.setName(patientRequestDTO.getName());
+        patient.setAddress(patientRequestDTO.getAddress());
+        patient.setEmail(patientRequestDTO.getEmail());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+        patient.setRegisteredDate(LocalDate.parse(patientRequestDTO.getResgisteredDate()));
+
+        return patient;
     }
 }
