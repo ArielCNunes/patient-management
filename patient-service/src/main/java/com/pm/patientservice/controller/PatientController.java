9918318105ea468a7957a632b13a8b7d320d727e
Ariel <arielc.nunes@outlook.com>
2025-09-29 19:47:18 +0100

    Implement DELETE endpoint

--- patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
@@ -42,4 +42,9 @@ public class PatientController {
         return ResponseEntity.ok().body(patientResponseDTO);
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePatient(@PathVariable UUID id) {
+        patientService.deletePatient(id);
+        return ResponseEntity.noContent().build(); // 204 No Content
+    }
 }
